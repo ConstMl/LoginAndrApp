b6ff2f3229e1a63eaaf83aeaf1093d93b78f6087
ConstML <k-malinov@bk.ru>
2021-06-24 14:09:28 +0300

    add navigation

---
+++ app/src/main/java/ru/skillbranch/loginandrapp/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package ru.skillbranch.loginandrapp
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/ru/skillbranch/loginandrapp/WelcomeViewModel.kt
@@ -0,0 +1,7 @@
+package ru.skillbranch.loginandrapp
+
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

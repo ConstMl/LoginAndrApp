3e8a8dbc0b0b18ea16ca5002f0d61ec933e2aff8
ConstML <k-malinov@bk.ru>
2021-06-28 17:58:25 +0300

    add two-way binding

--- app/src/main/java/ru/skillbranch/loginandrapp/model/viewmodel/LoginViewModel.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/model/viewmodel/LoginViewModel.kt
@@ -3,12 +3,15 @@ package ru.skillbranch.loginandrapp.model.viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ru.skillbranch.loginandrapp.model.LoginModel
+import kotlin.system.exitProcess
 
 class LoginViewModel(private val loginModel: LoginModel = LoginModel()) : ViewModel() {
 
-    var email = MutableLiveData("qwe1")
-    var password = MutableLiveData("ww")
+    var email = MutableLiveData("")
+    var password = MutableLiveData("")
     var error = MutableLiveData<String>()
     var isLoading = MutableLiveData<Boolean>()
 
@@ -16,4 +19,6 @@ class LoginViewModel(private val loginModel: LoginModel = LoginModel()) : ViewMo
         Log.d("onClickSignIn: ", "login: ${email.value}, pass: ${password.value}")
 //        this.loginModel.signIn()
     }
+
+    fun exitApp(): Unit = exitProcess(0)
 }
--- app/src/main/java/ru/skillbranch/loginandrapp/presentation/fragment/LoginFragment.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/presentation/fragment/LoginFragment.kt
@@ -1,20 +1,14 @@
 package ru.skillbranch.loginandrapp.presentation.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import ru.skillbranch.loginandrapp.model.viewmodel.LoginViewModel
 import ru.skillbranch.loginandrapp.R
 import ru.skillbranch.loginandrapp.databinding.LoginFragmentBinding
-import kotlin.system.exitProcess
-
-//import kotlinx.android.synthetic.main.login_fragment.*
 
 @Suppress("DEPRECATION")
 class LoginFragment : Fragment() {
@@ -36,24 +30,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = LoginFragmentBinding.bind(view)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         binding.viewModel = viewModel
-    }
-
-
-    override fun onStart() {
-        super.onStart()
-        binding.btnSignIn.setOnClickListener{
-            Log.d("----->", "{ ${binding.viewModel?.email?.value}, ${binding.viewModel?.password?.value} }")
-            Log.d("----->", "{ ${viewModel.email.value}, ${viewModel.password.value} }")
-            viewModel.signIn()
-//            (activity as MainActivity).navController.navigate(R.id.action_loginFragment_to_welcomeFragment)
-        }
-        binding.btnExit.setOnClickListener{
-            exitProcess (0);
-        }
+        binding.lifecycleOwner = this
     }
 }

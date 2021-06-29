55bc193c215b17b2bb471087684eefc437a764ab
ConstML <k-malinov@bk.ru>
2021-06-29 12:47:59 +0300

    move params LoginModel

--- app/src/main/java/ru/skillbranch/loginandrapp/model/LoginModel.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/model/LoginModel.kt
@@ -3,11 +3,9 @@ package ru.skillbranch.loginandrapp.model
 import ru.skillbranch.loginandrapp.data.AuthorizationApiService
 
 class LoginModel(
-    private val email: String? = "",
-    private val password: String? = "",
     private val authorizationService: AuthorizationApiService = AuthorizationApiService.create()
 ) {
-    suspend fun signIn(): Boolean {
+    suspend fun signIn(email: String? = "", password: String? = ""): Boolean {
         return authorizationService.signIn(email, password)
     }
 }

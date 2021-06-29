5b0501103021037cb8030813498234b88b214ee3
ConstML <k-malinov@bk.ru>
2021-06-29 15:29:37 +0300

    create imitate sign in

--- app/src/main/java/ru/skillbranch/loginandrapp/model/viewmodel/LoginViewModel.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/model/viewmodel/LoginViewModel.kt
@@ -3,6 +3,10 @@ package ru.skillbranch.loginandrapp.model.viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
 import ru.skillbranch.loginandrapp.model.LoginModel
 import kotlin.system.exitProcess
 
@@ -15,7 +19,10 @@ class LoginViewModel(private val loginModel: LoginModel) : ViewModel() {
 
     fun signIn() { // suspend
         Log.d("onClickSignIn: ", "login: ${email.value}, pass: ${password.value}")
-//        this.loginModel.signIn()
+        viewModelScope.launch(Dispatchers.Main) {
+            loginModel.signIn(email = email.value, password = password.value)
+        }
+//        this.loginModel.signIn(email = email.value, password = password.value)
     }
 
     fun exitApp(): Unit = exitProcess(0)

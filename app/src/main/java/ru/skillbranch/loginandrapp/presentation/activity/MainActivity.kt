8f602c2d2c79bc9a28a7eea7dbb052076b159f98
ConstML <k-malinov@bk.ru>
2021-06-30 14:54:51 +0300

    create navModule koin

--- app/src/main/java/ru/skillbranch/loginandrapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/presentation/activity/MainActivity.kt
@@ -6,10 +6,12 @@ import android.widget.Toast
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import androidx.navigation.NavDeepLinkRequest
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
 import ru.skillbranch.loginandrapp.R
 import ru.skillbranch.loginandrapp.data.Navigator
+import ru.skillbranch.loginandrapp.navModule
 
 class MainActivity : AppCompatActivity(), Navigator {
 
@@ -20,6 +22,12 @@ class MainActivity : AppCompatActivity(), Navigator {
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
+        loadKoinModules(navModule)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unloadKoinModules(navModule)
     }
 
     override fun navigate(url: String, args: Bundle?) {

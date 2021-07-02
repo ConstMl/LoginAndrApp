219bd2aad1150d33f3fae16c3ff1cf4b0c9aed2f
ConstML <k-malinov@bk.ru>
2021-07-02 13:32:31 +0300

    refactor

--- app/src/main/java/ru/skillbranch/loginandrapp/data/AuthorizationApiService.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/data/AuthorizationApiService.kt
@@ -1,12 +1,5 @@
 package ru.skillbranch.loginandrapp.data
 
-import android.util.Log
-import kotlinx.coroutines.delay
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Query
-import retrofit2.http.*
-
 interface AuthorizationApiService {
     suspend fun signIn(login: String?, password: String?) : Boolean
 }

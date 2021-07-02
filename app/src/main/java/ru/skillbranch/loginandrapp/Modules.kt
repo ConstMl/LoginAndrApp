3f380f4b33a8d4b8518ee24354490a99a7fb5448
ConstML <k-malinov@bk.ru>
2021-07-02 13:23:08 +0300

    add DI for AuthService

--- app/src/main/java/ru/skillbranch/loginandrapp/Modules.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/Modules.kt
@@ -1,14 +1,14 @@
 package ru.skillbranch.loginandrapp
 
 import org.koin.dsl.module
-import org.koin.dsl.single
 import org.koin.androidx.viewmodel.dsl.viewModel
 import ru.skillbranch.loginandrapp.data.AuthorizationApiService
+import ru.skillbranch.loginandrapp.data.SingleAuthorizationApiService
 import ru.skillbranch.loginandrapp.model.LoginModel
 import ru.skillbranch.loginandrapp.model.viewmodel.LoginViewModel
 
 val loginModule = module {
     viewModel { LoginViewModel(get(), get()) }
-    factory { LoginModel(AuthorizationApiService.instance) }
-    single<AuthorizationApiService>()
+    factory { LoginModel(get()) }
+    single<AuthorizationApiService> { SingleAuthorizationApiService() }
 }
--- app/src/main/java/ru/skillbranch/loginandrapp/data/AuthorizationApiService.kt
+++ app/src/main/java/ru/skillbranch/loginandrapp/data/AuthorizationApiService.kt
@@ -8,39 +8,5 @@ import retrofit2.http.Query
 import retrofit2.http.*
 
 interface AuthorizationApiService {
-    suspend fun signIn(login: String?, password: String?) : Boolean {
-        delay(1500)
-        Log.d("AuthorizationApiService", "login: $login, pass: $password")
-        return login == "login"
-    }
-
-    companion object SingleAuthorizationApiService : AuthorizationApiService {
-        private var authorizationApiService: AuthorizationApiService? = null
-        val instance: AuthorizationApiService
-            get() {
-                if (authorizationApiService == null) authorizationApiService = create()
-                return authorizationApiService!!
-            }
-        private fun create(): AuthorizationApiService {
-            return AuthorizationApiService
-        }
-    }
-
-//    companion object SingleAuthorizationApiService {
-//        private var authorizationApiService: AuthorizationApiService? = null
-//
-//        val instance: AuthorizationApiService
-//            get() {
-//                if (authorizationApiService == null) authorizationApiService = create()
-//                return authorizationApiService!!
-//            }
-//
-//        private fun create(): AuthorizationApiService {
-//            val retrofit = Retrofit.Builder()
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .baseUrl("https://qwe.qwe.qwe/api/v3/")
-//                .build()
-//            return retrofit.create(AuthorizationApiService::class.java)
-//        }
-//    }
+    suspend fun signIn(login: String?, password: String?) : Boolean
 }
